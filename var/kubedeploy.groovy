90c059afaa26f63838195ba17441761c345a0f6b
Avinash6666 <avinashbollepalli666@gmail.com>
2024-04-17 10:44:37 +0530

    K8S file

--- var/kubedeploy.groovy
+++ var/kubedeploy.groovy
@@ -1,4 +1,4 @@
 def call(){
     withKubeConfig(cacertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '')
-    sh 'kubectl apply -f deployment.yml'
+    sh "kubectl apply -f deployment.yml"
 }
