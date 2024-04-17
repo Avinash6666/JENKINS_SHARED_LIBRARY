d92ed2f4a9d533bb9909d388e7b782d9faa5b2b6
Avinash6666 <105978391+Avinash6666@users.noreply.github.com>
2024-04-17 10:46:42 +0530

    Update kubedelete.groovy

--- var/kubedelete.groovy
+++ var/kubedelete.groovy
@@ -1,4 +1,4 @@
 def call(){
     withKubeConfig(cacertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '')
-    sh 'kubectl delete -f deployment.yml'
+    sh "kubectl delete -f deployment.yml"
 }
